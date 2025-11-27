4a6b5baffa5d9fe9f1cbbd9626253e0743844521
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-27 22:21:10 +0100

    update

---
+++ models/User.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    default: 20
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User
