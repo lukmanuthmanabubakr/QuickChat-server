002d6abb8096c28f2a66d643ca28509953a01201
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-28 23:41:20 +0100

    update

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,4 +1,20 @@
-
-
+import Chat from "../models/Chat.js";
 
 //API Controller for creating a new chat
+export const createChat = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const chatData = {
+      userId,
+      messages: [],
+      name: "New Chat",
+      userName: req.user.name,
+    };
+
+    await Chat.create(chatData);
+    res.json({ success: true, message: "Chat created" });
+  } catch (error) {
+    res.json({success: false, error: error.message})
+  }
+};
