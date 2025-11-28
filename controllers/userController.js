6f6bbf4a0f537da505b412a1bdb4049f1f6b3306
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-28 09:22:09 +0100

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,10 @@
 import User from "../models/User";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
 
 //API to register user
 export const registerUser = async (req, res) => {
@@ -6,9 +12,39 @@ export const registerUser = async (req, res) => {
   try {
     const userExists = await User.findOne({ email });
 
-    if(userExists){
-        return res.json({success: false, message: "User already exists"})
+    if (userExists) {
+      return res.json({ success: false, message: "User already exists" });
     }
-    const user = await User.create({name, email, password})
-  } catch (error) {}
+    const user = await User.create({ name, email, password });
+
+    const token = generateToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//API to register user
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        const token = generateToken(user._id);
+        return res.json({ success: true, token });
+      }
+    }
+    return res.json({success: false, message: "Invalid email or password"})
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
 };
