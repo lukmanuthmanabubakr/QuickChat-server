97eb5ef59cd014cdd75b9ddf5d4ad2429b0e08f7
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-30 00:28:59 +0100

    update

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,11 +1,18 @@
 import axios from "axios";
 import Chat from "../models/Chat.js";
 import User from "../models/User.js";
+import imagekit from "../configs/imageKit.js";
 
 //Controller based on text AI CHAT
 export const textMessageController = async (req, res) => {
   try {
     const userId = req.user._id;
+    if (req.user.credits < 1) {
+      return res.json({
+        success: false,
+        message: "You don't have enough credits to use this features",
+      });
+    }
     const { chatId, prompt } = req.body;
 
     const chat = await Chat.findOne({ userId, _id: chatId });
@@ -73,10 +80,37 @@ export const imageMessageController = async (req, res) => {
       process.env.IMAGEKIT_URL_ENDPOINT
     }/ik-genimg-prompt-${encodedPrompt}/quictgpt/${Date.now()}.png?tr=w800,h-800`;
 
-
     //Trigger generation by fetching from Imagekit
     const aiImageResponse = await axios.get(generatedImageUrl, {
       responseType: "arraybuffer",
     });
-  } catch (error) {}
+
+    //Convert to base64
+    const base64Image = `data:image/png;base64,${Buffer.from(
+      aiImageResponse.data,
+      "binary"
+    ).toString("base64")}`;
+
+    //Upload to ImageKit Media Libary
+    const uploadResponse = await imagekit.upload({
+      file: base64Image,
+      fileName: `${Date.now()}.png`,
+      folder: "quickgpt",
+    });
+
+    const reply = {
+      role: "assistant",
+      content: uploadResponse.url,
+      timestamp: Date.now(),
+      isImage: true,
+      isPublished,
+    };
+    res.json({ success: true, reply });
+
+    chat.messages.push(reply);
+    await chat.save();
+    await User.updateOne({ _id: userId }, { $inc: { credits: -2 } });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import connectDB from "./configs/db.js"
 import userRouter from "./routes/userRoutes.js"
 import chatRouter from "./routes/chatRoute.js"
+import messageRouter from "./routes/messageRoute.js"
 
 const app = express()
 
@@ -17,6 +18,7 @@ app.use(express.json())
 app.get('/', (req, res) => res.send('Ai server is running...'))
 app.use('/api/user', userRouter)
 app.use('/api/chat', chatRouter)
+app.use('/api/message', messageRouter)
 
 const PORT = process.env.PORT || 3590
 
