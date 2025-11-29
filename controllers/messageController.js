7d40c4f59b8d1a7e95963de9969816a516b95aa4
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-29 08:47:23 +0100

    update

---
+++ controllers/messageController.js
@@ -0,0 +1,17 @@
+import Chat from "../models/Chat";
+
+//Controller based on text based AI CHAT
+export const textMessageController = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { chatId, prompt } = req.body;
+
+    const chat = await Chat.findOne({ userId, _id: chatId });
+    chat.messages.push({
+      role: "user",
+      content: prompt,
+      timestamp: Date.now(),
+      isImage: false,
+    });
+  } catch (error) {}
+};
